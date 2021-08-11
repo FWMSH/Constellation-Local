a27b8ecf18aa3fe8055ad976df9b8d1ca6ea8a57
Morgan Rehnberg <forceflow1049@gmail.com>
2021-08-11 14:22:12 -0500

    Final content addition

--- 9-11_kiosk/2_attack/content.js
+++ 9-11_kiosk/2_attack/content.js
@@ -105,5 +105,98 @@ var timelineContentList = [
     body_en: "The pilots do not understand the warning and ask for clarification.",
     body_es: "Spanish body",
     rank: 4
-  }
+  },
+  {
+    title_en: "UA93 is hijacked",
+    title_es: "Spanish title",
+    time_en: "9:28 AM",
+    time_es: "9:28 AM",
+    body_en: "Four hijackers seize control of the plane.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "UA93 passengers learn of WTC attacks",
+    title_es: "Spanish title",
+    time_en: "9:30 AM",
+    time_es: "9:30 AM",
+    body_en: "In phone calls with family on the ground, passengers on Flight 93 learn that two hijacked planes have already been crashed. They realize that their situation is likely connected, and begin planning to retake the plane.",
+    body_es: "Spanish body",
+    rank: 4
+  },
+  {
+    title_en: "AA77 strikes the Pentagon",
+    title_es: "Spanish title",
+    time_en: "9:37 AM",
+    time_es: "9:37 AM",
+    body_en: "The plane strikes a the building at 530 mph (850 kph). It impacts a recently-renovated portion of the building, which is still mostly unoccupied. Even so, 125 people in the Pentagon are killed.",
+    body_es: "Spanish body",
+    caption_en: "The aftermath of AA77's impact on the Pentagon.",
+    caption_es: "Spanish caption",
+    image: "Pentagon_aftermath_Public_Domain.JPEG",
+    rank: 1
+  },
+  {
+    title_en: "President Bush boards Air Force One",
+    title_es: "Spanish title",
+    time_en: "9:55 AM",
+    time_es: "9:55 AM",
+    body_en: "Air Force One lifts off in Florida to ensure the President's safety. They do not have a plan about where to go.",
+    body_es: "Spanish body",
+    caption_en: "President Bush aboard Air Force One on September 11.",
+    caption_es: "Spanish caption",
+    image: "President_George_W._Bush_aboard_Air_Force_One_Public_Domain.jpg",
+    rank: 4
+  },
+  {
+    title_en: "UA93 passengers revolt",
+    title_es: "Spanish title",
+    time_en: "9:57 AM",
+    time_es: "9:57 AM",
+    body_en: "After voting on whether to act, passengers rush from the rear of the plane towards the front. The hijackers begin tipping the plane forwards and backwards in defense, as passengers begin trying to break into the cockpit. It remains unclear if they succeeded.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "South Tower falls",
+    title_es: "Spanish title",
+    time_en: "9:59 AM",
+    time_es: "9:59 AM",
+    body_en: "",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "UA93 crashes",
+    title_es: "Spanish title",
+    time_en: "10:03 AM",
+    time_es: "10:03 AM",
+    body_en: "As passengers rush the cockpit and struggle to regain control of the plane, the hijackers deliberately crash it in a field in southwestern Pennslyvania. Flight 93 was about 20 minutes from reaching Washington, D.C.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "North Tower falls",
+    title_es: "Spanish title",
+    time_en: "10:28 AM",
+    time_es: "10:28 AM",
+    body_en: "After burning for 1 hour and 42 minutes, the North Tower collapses. Because the impact severed all three stairwells, no one above the impact site escapes. A worldwide audience views the collapse live on television.",
+    body_es: "Spanish body",
+    caption_en: "<i>From bottom to top:</i> The ruins of the North Tower, 6 WTC, and 7 WTC.",
+    caption_es: "Spanish caption",
+    image: "WTC_1_6_7_ruins_Public_Domain.jpg",
+    rank: 2
+  },
+  {
+    title_en: "7 WTC falls",
+    title_es: "Spanish title",
+    time_en: "5:21 PM",
+    time_es: "5:21 PM",
+    body_en: "",
+    body_es: "Spanish body",
+    caption_en: "7 WTC on fire before its collapse.",
+    caption_es: "Spanish caption",
+    image: "Wtc7onfire_Public_Domain.jpg",
+    rank: 3
+  },
 ];
