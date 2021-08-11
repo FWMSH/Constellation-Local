626e2aeda21e00190c3092db166a6d4d19faac0e
Morgan Rehnberg <forceflow1049@gmail.com>
2021-08-11 13:33:49 -0500

    Programmatically create timeline from list of elements

---
+++ 9-11_kiosk/2_attack/content.js
@@ -0,0 +1,15 @@
+var timelineContentList = [
+  {
+    title_en: "President Bush arrives at Emma E. Booker Elementary School",
+    title_es: "Spanish title",
+    time_en: "8:55 AM",
+    time_es: "8:55 AM",
+    body_en: "The President is reading a story to students when he first learns of the attacks.",
+    body_es: "Spanish body",
+    caption_en: "President Bush reading <i>The Pet Goat</i> to a class of elementary school students at Emma E. Booker Elementary School.",
+    caption_es: "Spanish caption",
+    image: "Bush_reading_Public_Domain.jpg",
+    credit: "",
+    rank: 4
+  }
+]
