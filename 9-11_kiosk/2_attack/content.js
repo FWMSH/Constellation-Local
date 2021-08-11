890e125276f304c9f58f41a1ebf784f860a53726
Morgan Rehnberg <forceflow1049@gmail.com>
2021-08-11 13:53:41 -0500

    Moving content to new timeline system

--- 9-11_kiosk/2_attack/content.js
+++ 9-11_kiosk/2_attack/content.js
@@ -1,5 +1,77 @@
 var timelineContentList = [
   {
+    title_en: "AA11 departs Boston, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "7:59 AM",
+    time_es: "7:59 AM",
+    body_en: "On board the Boeing 767 are 81 passengers and 11 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA175 departs Boston, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "8:14 AM",
+    time_es: "8:14 AM",
+    body_en: "On board the Boeing 767 are 56 passengers and 9 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "AA11 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:14 AM",
+    time_es: "8:14 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane south.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "AA77 departs Washington, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "8:20 AM",
+    time_es: "8:20 AM",
+    body_en: "On board the Boeing 757 are 58 passengers and 6 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA93 departs Newark, headed for San Francisco",
+    title_es: "Spanish title",
+    time_en: "8:42 AM",
+    time_es: "8:42 AM",
+    body_en: "On board the Boeing 757 are 37 passengers and 7 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA175 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:43 AM",
+    time_es: "8:43 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane to the northeast.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "AA11 strikes the North Tower",
+    title_es: "Spanish title",
+    time_en: "8:46 AM",
+    time_es: "8:46 AM",
+    body_en: "",
+    body_es: "Spanish body",
+    rank: 1
+  },
+  {
+    title_en: "AA77 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:50 AM",
+    time_es: "8:50 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane to the southeast.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
     title_en: "President Bush arrives at Emma E. Booker Elementary School",
     title_es: "Spanish title",
     time_en: "8:55 AM",
@@ -11,5 +83,27 @@ var timelineContentList = [
     image: "Bush_reading_Public_Domain.jpg",
     credit: "",
     rank: 4
+  },
+  {
+    title_en: "UA175 strikes the South Tower",
+    title_es: "Spanish title",
+    time_en: "9:03 AM",
+    time_es: "9:03 AM",
+    body_en: "The plane strikes at 590 mph (950 kph) between floors 77 and 85.",
+    body_es: "Spanish body",
+    caption_en: "",
+    caption_es: "Spanish caption",
+    image: "UA_Flight_175_hits_WTC_south_tower_9-11_CC BY-SA_Robert_J_Fisch.jpeg",
+    credit: "Robert J. Fisch",
+    rank: 1
+  },
+  {
+    title_en: "UA93 warned of earlier hijackings",
+    title_es: "Spanish title",
+    time_en: "9:24 AM",
+    time_es: "9:24 AM",
+    body_en: "The pilots do not understand the warning and ask for clarification.",
+    body_es: "Spanish body",
+    rank: 4
   }
-]
+];
