eadef71d6a948cdf6b09cfed1986b35f01ad23cc
Morgan Rehnberg <forceflow1049@gmail.com>
2021-09-01 11:43:21 -0500

    Clone attack to cover other sections

---
+++ 9-11_kiosk/4_military/content.js
@@ -0,0 +1,302 @@
+var timelineContentList = [
+  {
+    title_en: "AA11 departs Boston, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "7:59 AM",
+    time_es: "7:59 AM",
+    body_en: "On board the Boeing 767 are 81 passengers and 11 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA175 departs Boston, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "8:14 AM",
+    time_es: "8:14 AM",
+    body_en: "On board the Boeing 767 are 56 passengers and 9 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "AA11 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:14 AM",
+    time_es: "8:14 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane south.",
+    body_es: "Spanish body",
+    rank: 2,
+  },
+  {
+    title_en: "AA77 departs Washington, headed for Los Angeles",
+    title_es: "Spanish title",
+    time_en: "8:20 AM",
+    time_es: "8:20 AM",
+    body_en: "On board the Boeing 757 are 58 passengers and 6 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA93 departs Newark, headed for San Francisco",
+    title_es: "Spanish title",
+    time_en: "8:42 AM",
+    time_es: "8:42 AM",
+    body_en: "On board the Boeing 757 are 37 passengers and 7 crew.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "UA175 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:43 AM",
+    time_es: "8:43 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane to the northeast.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "AA11 strikes the North Tower",
+    title_es: "Spanish title",
+    time_en: "8:46 AM",
+    time_es: "8:46 AM",
+    body_en: "",
+    body_es: "Spanish body",
+    rank: 1
+  },
+  {
+    title_en: "AA77 is hijacked",
+    title_es: "Spanish title",
+    time_en: "8:50 AM",
+    time_es: "8:50 AM",
+    body_en: "Five hijackers gain control of the aircraft, turning the plane to the southeast.",
+    body_es: "Spanish body",
+    rank: 3
+  },
+  {
+    title_en: "President Bush arrives at Emma E. Booker Elementary School",
+    title_es: "Spanish title",
+    time_en: "8:55 AM",
+    time_es: "8:55 AM",
+    body_en: "The President is reading a story to students when he first learns of the attacks.",
+    body_es: "Spanish body",
+    caption_en: "President Bush reading <i>The Pet Goat</i> to a class of elementary school students at Emma E. Booker Elementary School.",
+    caption_es: "Spanish caption",
+    image: "Bush_reading_Public_Domain.jpg",
+    credit: "",
+    rank: 4
+  },
+  {
+    title_en: "UA175 strikes the South Tower",
+    title_es: "Spanish title",
+    time_en: "9:03 AM",
+    time_es: "9:03 AM",
+    body_en: "The plane strikes at 590 mph (950 kph) between floors 77 and 85.",
+    body_es: "Spanish body",
+    caption_en: "",
+    caption_es: "Spanish caption",
+    image: "UA_Flight_175_hits_WTC_south_tower_9-11_CC BY-SA_Robert_J_Fisch.jpeg",
+    credit: "Robert J. Fisch",
+    rank: 1
+  },
+  {
+    title_en: "UA93 warned of earlier hijackings",
+    title_es: "Spanish title",
+    time_en: "9:24 AM",
+    time_es: "9:24 AM",
+    body_en: "The pilots do not understand the warning and ask for clarification.",
+    body_es: "Spanish body",
+    rank: 4,
+    link: "Fighting_chance_para_1"
+  },
+  {
+    title_en: "UA93 is hijacked",
+    title_es: "Spanish title",
+    time_en: "9:28 AM",
+    time_es: "9:28 AM",
+    body_en: "Four hijackers seize control of the plane.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "UA93 passengers learn of WTC attacks",
+    title_es: "Spanish title",
+    time_en: "9:30 AM",
+    time_es: "9:30 AM",
+    body_en: "In phone calls with family on the ground, passengers on Flight 93 learn that two hijacked planes have already been crashed. They realize that their situation is likely connected, and begin planning to retake the plane.",
+    body_es: "Spanish body",
+    rank: 4
+  },
+  {
+    title_en: "AA77 strikes the Pentagon",
+    title_es: "Spanish title",
+    time_en: "9:37 AM",
+    time_es: "9:37 AM",
+    body_en: "The plane strikes a the building at 530 mph (850 kph). It impacts a recently-renovated portion of the building, which is still mostly unoccupied. Even so, 125 people in the Pentagon are killed.",
+    body_es: "Spanish body",
+    caption_en: "The aftermath of AA77's impact on the Pentagon.",
+    caption_es: "Spanish caption",
+    image: "Pentagon_aftermath_Public_Domain.JPEG",
+    rank: 1
+  },
+  {
+    title_en: "President Bush boards Air Force One",
+    title_es: "Spanish title",
+    time_en: "9:55 AM",
+    time_es: "9:55 AM",
+    body_en: "Air Force One lifts off in Florida to ensure the President's safety. They do not have a plan about where to go.",
+    body_es: "Spanish body",
+    caption_en: "President Bush aboard Air Force One on September 11.",
+    caption_es: "Spanish caption",
+    image: "President_George_W._Bush_aboard_Air_Force_One_Public_Domain.jpg",
+    rank: 4
+  },
+  {
+    title_en: "UA93 passengers revolt",
+    title_es: "Spanish title",
+    time_en: "9:57 AM",
+    time_es: "9:57 AM",
+    body_en: "After voting on whether to act, passengers rush from the rear of the plane towards the front. The hijackers begin tipping the plane forwards and backwards in defense, as passengers begin trying to break into the cockpit. It remains unclear if they succeeded.",
+    body_es: "Spanish body",
+    rank: 2,
+    link: "Struggle_para_1"
+  },
+  {
+    title_en: "South Tower falls",
+    title_es: "Spanish title",
+    time_en: "9:59 AM",
+    time_es: "9:59 AM",
+    body_en: "",
+    body_es: "Spanish body",
+    rank: 2,
+    link: "Collapse_para_1"
+  },
+  {
+    title_en: "UA93 crashes",
+    title_es: "Spanish title",
+    time_en: "10:03 AM",
+    time_es: "10:03 AM",
+    body_en: "As passengers rush the cockpit and struggle to regain control of the plane, the hijackers deliberately crash it in a field in southwestern Pennslyvania. Flight 93 was about 20 minutes from reaching Washington, D.C.",
+    body_es: "Spanish body",
+    rank: 2
+  },
+  {
+    title_en: "North Tower falls",
+    title_es: "Spanish title",
+    time_en: "10:28 AM",
+    time_es: "10:28 AM",
+    body_en: "After burning for 1 hour and 42 minutes, the North Tower collapses. Because the impact severed all three stairwells, no one above the impact site escapes. A worldwide audience views the collapse live on television.",
+    body_es: "Spanish body",
+    caption_en: "<i>From bottom to top:</i> The ruins of the North Tower, 6 WTC, and 7 WTC.",
+    caption_es: "Spanish caption",
+    image: "WTC_1_6_7_ruins_Public_Domain.jpg",
+    rank: 2
+  },
+  {
+    title_en: "7 WTC falls",
+    title_es: "Spanish title",
+    time_en: "5:21 PM",
+    time_es: "5:21 PM",
+    body_en: "",
+    body_es: "Spanish body",
+    caption_en: "7 WTC on fire before its collapse.",
+    caption_es: "Spanish caption",
+    image: "Wtc7onfire_Public_Domain.jpg",
+    rank: 3
+  },
+];
+
+var bodyContentList = [
+  {
+    id: "Flights_para_1",
+    header_en: "Four Fated Flights",
+    header_es: "Spanish header",
+    body_en: "At 7:59 AM, a Boeing 767 jetliner lifts off from Boston, headed to Los Angeles. Within the next 45 minutes, three more planes—another 767 and two Boeing 757s—would also depart from airports on the East Coast to destinations on the West. In total, nineteen hijackers were on board the four aircraft. Al-Qaeda had not chosen these planes at random—the 757 and 767 were the only commercial aircraft to share a cockpit layout, ensuring each hijacker could be trained in the same way. The planes were also large aircraft typically used for international travel; their West Coast destinations ensured that they were carrying a full load of fuel. ",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Hijacking_para_1",
+    header_en: "No Ordinary Hijacking",
+    header_es: "Spanish header",
+    body_en: "Hijackings were commonplace in the 20th century. In just the five years leading up to 2001, 76 planes were seized in flight around the world. The vast majority of these incidents played out as hostage situations, and most victims weren’t physically harmed. In many cases, the attackers used the plane as transportation to an unlawful destination, such as Cuba. In others, the passengers were ransomed back to their government for money or the release of prisoners. For passengers, the rules were simple: don’t fight back and things would usually turn out okay.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Hijacking_para_2",
+    body_en: "As hijackers seize American Airlines Flight 11 at 8:14 AM, the situation seems to be playing out exactly as it had for decades. The hijackers attack two flight attendants and one passenger who rise to confront them, but most sit quietly as the crew treat the wounded. Two more flight attendants use crew telephones to contact managers on the ground and relay information. One reports being shown a bomb. It is a very bad morning, but nothing the airline hadn’t faced before.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Hijacking_para_3",
+    body_en: "Then, at 8:46 AM, everything changes. The telephone connection with AA11 goes dead. Moments later, American Airlines receives news: an aircraft has just collided with the World Trade Center. ",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Confusion_para_1",
+    header_en: "A World of Confusion",
+    header_es: "Spanish header",
+    body_en: "The events of 9/11 were unparalleled in their complexity. Four aircraft left three airports headed for two destinations before being diverted towards three targets—all in the same airspace, all within a two-hour window. With a top speed of around 600 mph (965 kph), a Boeing 757 could travel from Boston to New York City in less than half an hour. ",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Confusion_para_2",
+    body_en: "A vast array of oversight complicated and slowed the nation’s response to the unfolding disaster. Various pieces of critical information lay in the hands of the airlines, civilian air traffic controllers, military aerospace defense coordinators, and elected decisionmakers. Time and again on September 11, vital information passed from one organization to the next too slowly to be useful. At the same time, the terrorists’ careful planning and coordination ensured that the windows in which lifesaving decisions could be made were impossibly small.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Confusion_para_3",
+    body_en: "Around 8:46 AM, two F-15 fighters take off to intercept AA11. It crashes into the World Trade Center eight seconds before they are airborne.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "More_hijacking_para_1",
+    header_en: "A Second Hijacking, and Then a Third",
+    header_es: "Spanish header",
+    body_en: "At 8:43 AM—three minutes before AA11 would strike the North Tower—five hijackers seize United Airlines Flight 175. Seven minutes later, five others gain control of American Flight 77. It’s not until after the third plane is hijacked that President Bush learns of even the first event. He’s at an elementary school in Florida, preparing to spend the day promoting his education agenda.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "More_hijacking_para_2",
+    body_en: "It takes only 20 minutes for the terrorists to steer UA175 into the South Tower of the World Trade Center. The plane strikes just minutes after television networks switch to live coverage of AA11’s crash. Millions of viewers see the impact live on TV.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "More_hijacking_para_3",
+    body_en: "By 9:33 AM, air traffic controllers in Washington, D.C. are warning the Secret Service that AA77 appears headed for the White House. Then, it abruptly changes direction. Five minutes later, it strikes the Pentagon. In a rare stroke of luck, the plane hits a part of the massive building that is under renovation. Of tens of thousands of employees, just 125 are killed.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Fighting_chance_para_1",
+    header_en: "A Fighting Chance",
+    header_es: "Spanish header",
+    body_en: `Of all the hijacked aircraft, only United Airlines Flight 93 had any chance to react. At 9:24 AM, the flight receives a brief message from United headquarters: "Beware any cockpit intrusion—Two [aircraft] hit World Trade Center." But, without context, the pilots don’t understand the warning. They respond, asking for clarification. Four minutes later, four attackers storm the cockpit and seize control of the plane.`,
+    body_es: "Spanish body"
+  },
+  {
+    id: "Fighting_chance_para_2",
+    body_en: "The pilots aren’t the only ones to receive a warning. Like on other hijacked flights, passengers make phone calls to the ground after the hijacking. But, by the time of the UA93 hijacking, the events at the World Trade Center are already global news. Within minutes, the first passengers learn that their plane, too, is likely on a suicide mission. Their only chance is to act.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Struggle_para_1",
+    header_en: `"Let's roll"`,
+    header_es: "Spanish header",
+    body_en: `Huddled at the back of the plane, UA93’s passengers take a vote on whether to charge the cockpit. One flight attendant boils a pot of water to use as a weapon. Others may have readied the food service cart, or looked for other makeshift tools. At 9:57 AM, they’re ready. Moments later, a struggle erupts outside the cockpit, as passengers fight to get in. The hijacker piloting the plane beings to tilt it violently, trying to knock the fighting passengers off their feet. When it’s clear that the passengers will soon be successful, the hijackers elect to fly the plane straight into the ground.`,
+    body_es: "Spanish body"
+  },
+  {
+    id: "Struggle_para_2",
+    body_en: "The plane crashes six minutes after the struggle begins, landing in a field in Somerset County, Pennsylvania. Interrogations in the years following 9/11 revealed that UA93’s intended target was most likely the U.S. Capitol building in Washington, D.C. At the speed of a Boeing 757, the flight would have reached Washington only fifteen minutes later.",
+    body_es: "Spanish body"
+  },
+  {
+    id: "Collapse_para_1",
+    header_en: `Collapse`,
+    header_es: "Spanish header",
+    body_en: `The crash of UA93 marked the end of the hijackings. But, in New York City, the scale of the disaster was only starting to come into focus. After burning for nearly an hour, the impact zone on the South Tower of the World Trade Center gives way. Structural elements within the building were designed to support one another; As fire weakens the beams not destroyed on impact, they begin to bend, fatally compromising the tower’s strength.`,
+    body_es: "Spanish body"
+  },
+  {
+    id: "Collapse_para_2",
+    body_en: "Half an hour later, the North Tower collapses as well. Debris from the falling towers damages or destroys nearby buildings, including others in the World Trade Center complex. Still, the buildings performed admirably in the face of events they were never designed to withstand. The vast majority of those who perished on 9/11 were trapped above the impact zone on each building; the towers remained standing long enough to enable the evacuation of nearly everyone located below where the planes struck.",
+    body_es: "Spanish body"
+  },
+]
