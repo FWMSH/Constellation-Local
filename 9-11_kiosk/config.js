52e4311a27af19f16546a79460391718d09e0f47
Morgan Rehnberg <forceflow1049@gmail.com>
2021-09-01 11:24:42 -0500

    Update config.js

--- 9-11_kiosk/config.js
+++ 9-11_kiosk/config.js
@@ -1,4 +1,3 @@
-// var serverAddress = "http://10.8.0.168:8083";
-var serverAddress = "http://localhost:8082";
+var serverAddress = "http://10.8.0.168:8083";
 var id = "9-11_DISP";
 var type = "KIOSK";
