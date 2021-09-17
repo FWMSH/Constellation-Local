1f71d5be30787aea7526b8282ceb2e3586db37f4
Morgan Rehnberg <forceflow1049@gmail.com>
2021-09-17 16:00:04 -0500

    Reorganization

--- 9-11_kiosk/content.js
+++ 9-11_kiosk/content.js
@@ -1,45 +1,45 @@
 var superTitle_text = {
   "en": "Explore the Events of",
   "es": "Spanish Super Title"
-}
+};
 
 var title_text = {
   "en": "September 11th, 2001",
   "es": "Spanish Title"
-}
+};
 
 var learn_more_button_text = {
   "en": "Learn More",
   "es": "Spanish Learn More"
-}
+};
 
 // These are reversed on purpose!
 var lang_toggle_text = {
   "en": "Español",
   "es": "English"
-}
+};
 
 var prologue_description = {
   "en": "In the 1970s, the World Trade Center emerged as a global icon of American power and prosperity. At the same time, the nation was facing new threats. Al-Qaeda and its leader, Osama bin Laden, began to target symbols of America around the world. As the country entered a new millennium, hints began to appear that al-Qaeda might strike closer to home than ever before. But could the U.S. put the pieces together in time?",
   "es": "Spanish prologue description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+};
 
 var attack_description = {
   "en": "On Tuesday, September 11, 2001, al-Qaeda terrorists seized four passenger jets traveling from the East Coast to the West Coast. Over the following two hours, they piloted three of these planes into targets in New York City and Arlington, Virginia. A fourth, likely headed towards Washington, D.C., crashed in Pennsylvania after a struggle with passengers on board. The coordinated attacks left America shocked and trying to understand what had just happened.",
   "es": "Spanish attack description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+};
 
 var response_description = {
   "en": "As the shock of the attacks rippled across the world, the nation reeled in confusion. Buit, within seconds of the first impact, brave men and women banded together to save who and what they could. Some were first responders; others were simply the first on the scene. Journalists tried to make sense what had happened, while countless citizens lined up to donate their blood. In ways big and small, Americans reacted with courage and determination.",
   "es": "Spanish reaction description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+};
 
 var military_description = {
   "en": "The American military response to 9/11 was almost immediate. What began as an effort to capture those responsible for the attacks transformed into a conflict to combat the elusive “axis of evil.” Twenty years later, over 7,000 U.S. troops, 177,000 coalition partners, and 335,000 civilians have been killed in the War on Terror. Today, terrorism remains a potent threat. As the Taliban retakes Afghanistan, the legacy of U.S. military intervention in the Middle East remains uncertain.",
   "es": "Spanish military description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+};
 
 var legacy_description = {
   "en": "English legacy description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   "es": "Spanish legacy description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+};
